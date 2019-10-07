2168bb7f48d4b23113e1dcefbcb4ddd11c94ea7a
Jed Smit <jedsmit1@gmail.com>
2019-10-06 20:03:23 -0700

    added main game code, centered buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,10 +14,16 @@ var rubyValue = 0;
 var lapisValue = 0;
 var amethystValue = 0;
 
-//functions
+var scoreTally = 0;
+var targetVal = 0;
+var winTally = 0;
+var lossTally = 0;
+
+//-----------------------------------------functions---------------------------
 //generates and displays target number
 function generateTargetNum() {
     result = Math.floor(Math.random() * (120 - 20)) + 19;
+    targetVal = result;
     targetNumber.text(result);
 };
 //generates random crystal value
@@ -30,22 +36,73 @@ function resetCrystals() {
     rubyValue = generateCrystalVal();
     lapisValue = generateCrystalVal();
     amethystValue = generateCrystalVal();
+    wins.text(winTally);
+    losses.text(lossTally);
 
     console.log(diamondValue);
     console.log(rubyValue);
     console.log(lapisValue);
     console.log(amethystValue);
 };
-
-resetCrystals();
-
-//on game start, generate a random number between 19-120
-//display random number in target number div 
-//generate a random number for each crystal button
-//when clicked, add the crystal's number to the player's total
-//after each click, check score against the target number
+//adds gem value to user's score
+function tallyScore(gemVal) {
+    scoreTally += gemVal;
+    userScore.text(scoreTally);
+};
+//checks score against target number
 //if score equals the target number, player wins, increment wins, new game
 //if score is less than the target number, repeat the click stuff
 //if score is more than the target number, player loses, increment losses, new game
+
+function scoreCheck() {
+    if (scoreTally === targetVal) {
+        winTally++;
+        mainGame();
+    } else if (scoreTally > targetVal) {
+        lossTally++;
+        mainGame();
+    };
+};
+
+
+
+
+//-----------------------------------------Main Game---------------------------
+function mainGame() {
+    scoreTally = 0;
+    userScore.text(scoreTally);
+    //on game start, generate a random number between 19-120 and display in target number div
+    generateTargetNum();
+
+    //generate a random number for each crystal button
+    resetCrystals();
+    //when clicked, add the crystal's number to the player's total
+    //after each click, check score against the target number
+    if (scoreTally < targetVal) {
+        diamond.on("click", function () {
+            tallyScore(diamondValue);
+            scoreCheck();
+        });
+
+        ruby.on("click", function () {
+            tallyScore(rubyValue);
+            scoreCheck();
+        });
+
+        lapis.on("click", function () {
+            tallyScore(lapisValue);
+            scoreCheck();
+        });
+
+        amethyst.on("click", function () {
+            tallyScore(amethystValue);
+            scoreCheck();
+        });
+    };
+
+};
+
+mainGame();
+
 
 
